8509a42f8604d2ac725a493e8e081a74cf18fa1a
Nando <kommandernando@outlook.com>
2024-10-10 11:00:28 -0400

    Finished Code challenge

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //write your code here
+const reverseString = (str) => {
+    let reversed = ""
+    for(let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i]
+        console.log(reversed)
+    }
+}
+
+reverseString("Something");
+
+const reverseZigZagString = (str) => {
+    let reverseZigZag = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (i % 2 === 0) {
+            reverseZigZag += str[i].toUpperCase();
+        } else {
+            reverseZigZag += str[i].toLowerCase();
+        }
+    }
+    return reverseZigZag;
+};
+
+reverseZigZagString("Something");
